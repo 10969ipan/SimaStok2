e6f1938f8ccb4bf0859ae442f4385eb7dc416c8a
10969ipan <arfianirfan48@gmail.com>
2025-11-23 12:05:32 +0700

    update

--- resources/views/backend/v_produk/index.blade.php
+++ resources/views/backend/v_produk/index.blade.php
@@ -28,7 +28,7 @@
         @if (session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
             <i class="fa fa-check-circle me-2"></i> {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            
         </div>
         @endif
 
